67eed2ace9620fc041b5f20f48eef81a11825c4f
Takuto Hashibe <m235586@hiroshima-u.ac.jp>
2023-07-12 02:32:34 +0000

    input developed

--- Othello.go
+++ Othello.go
@@ -4,53 +4,27 @@ import (
 	"fmt"
 )
 
-const (
-	Empty = 0
-	Black = 1
-	White = 2
-	Size  = 8
-)
-
-type Game struct {
-	board  [][]int
-	player int
-}
-
-func NewGame() *Game {
-	board := make([][]int, Size)
-	for i := range board {
-		board[i] = make([]int, Size)
-	}
-
-	// initialize board
-	board[3][3] = White
-	board[3][4] = Black
-	board[4][3] = Black
-	board[4][4] = White
-
-	return &Game{
-		board:  board,
-		player: Black,
-	}
-}
-func (g *Game) PrintBoard() {
-	fmt.Println("  a b c d e f g h")
-	for i := 0; i < Size; i++ {
-		fmt.Printf("%d ", i+1)
-		for j := 0; j < Size; j++ {
-			switch g.board[i][j] {
-			case Black:
-				fmt.Print("● ")
-			case White:
-				fmt.Print("○ ")
-			default:
-				fmt.Print(". ")
-			}
+// 座標を入力し、x座標、y座標を戻り値
+func input() [2]int {
+	var zahyostr string
+	for {
+		fmt.Println("input: ")
+		fmt.Scanln(&zahyostr)               // 例：2,1
+		x, _ := strconv.Atoi(zahyostr[0:1]) // xにx座標代入
+		y, _ := strconv.Atoi(zahyostr[2:3]) // yにy座標代入
+		if 1 <= x && x <= 8 && 1 <= y && y <= 8 {
+			var zahyoint [2]int = [2]int{x - 1, y - 1}
+			break
 		}
-		fmt.Println()
+
 	}
+	return zahyoint
 }
+
 func main() {
-	game := NewGame()
-	game.PrintBoard()
+	for {
+		var zahyoint [2]int = input()
+		_ = zahyoint
+
+	}
 }
--- input.go
+++ input.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+	//  "reflect"
+	"strconv"
+)
+
+func main() {
+	// 	for i := 0; ; i++{
+	//      if i%2 == 0 {
+	//         input()[0]
+	//      }
+	//      else{
+	//          input()[1]
+	//      }
+	// 	}
+	var zahyoint [2]int = input()
+	_ = zahyoint
+	fmt.Println(zahyoint[0])
+	fmt.Println(zahyoint[1])
+
+}
+
+// 座標を入力し、x座標、y座標を戻り値
+func input() [2]int {
+	var zahyostr string
+	fmt.Println("input: ")
+	fmt.Scanln(&zahyostr)               // 例：2,1
+	x, _ := strconv.Atoi(zahyostr[0:1]) // xにx座標代入
+	y, _ := strconv.Atoi(zahyostr[2:3]) // yにy座標代入
+	var zahyoint [2]int = [2]int{x, y}
+	return zahyoint
+}
