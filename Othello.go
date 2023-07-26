8643721ec276e69b177df888354716fa5798cc07
Takuto Hashibe <m235586@hiroshima-u.ac.jp>
2023-07-26 01:07:49 +0000

    basic Othello implemented

--- Othello.go
+++ Othello.go
@@ -26,28 +26,26 @@ func main() {
 	PrintBoard(board)
 
 	player := Black
-	fmt.Println("player:%d please input x,y\n", player)
-	var zahyoint [2]int = input()
-	var okeru bool = okeru_okenai(zahyoint[0], zahyoint[1], board, player)
-	if okeru == true {
-		board = komahaitti(zahyoint[0], zahyoint[1], board, player)
-	} else {
-		println("置けません")
+
+	for {
+		if player == Black {
+			fmt.Println("player: ● please input x,y")
+		} else {
+			fmt.Println("player: ○ please input x,y")
+		}
+		var zahyoint [2]int = input()
+		var okeru bool = okeru_okenai(zahyoint[0], zahyoint[1], board, player)
+		if okeru == true {
+			board = komahaitti(zahyoint[0], zahyoint[1], board, player)
+			board = hasamu(zahyoint[0], zahyoint[1], board, player)
+			PrintBoard(board)
+			player = player%2 + 1
+		} else {
+			PrintBoard(board)
+			println("Cannot place. Please try again.")
+		}
 	}
-	PrintBoard(board)
-	//else
-	// もう一回入力を求める
-	//配置されたコマを基に白黒反転
-	//PrintBoard()
-
 	//勝敗を判断
-	//手番交代 player = player % 2 + 1
-
-	// for {
-	// 	var zahyoint [2]int = input()
-	// 	_ = zahyoint
-
-	// }
 }
 
 //座標を入力し、x座標、y座標を戻り値
@@ -115,6 +113,104 @@ func okeru_okenai(x int, y int, board [8][8]int, player int) bool {
 		}
 	}
 	return false
+}
+
+func hasamu(x int, y int, board [8][8]int, player int) [8][8]int {
+	if Top(x, y, board, player) == true {
+		board = hanten(board, player, x, y, 1)
+	}
+	if TopRight(x, y, board, player) == true {
+		board = hanten(board, player, x, y, 2)
+	}
+	if Right(x, y, board, player) == true {
+		board = hanten(board, player, x, y, 3)
+	}
+	if BottomRight(x, y, board, player) == true {
+		board = hanten(board, player, x, y, 4)
+	}
+	if Bottom(x, y, board, player) == true {
+		board = hanten(board, player, x, y, 5)
+	}
+	if BottomLeft(x, y, board, player) == true {
+		board = hanten(board, player, x, y, 6)
+	}
+	if Left(x, y, board, player) == true {
+		board = hanten(board, player, x, y, 7)
+	}
+	if TopLeft(x, y, board, player) == true {
+		board = hanten(board, player, x, y, 8)
+	}
+	return board
+}
+
+func hanten(board [8][8]int, player int, y int, x int, direction int) [8][8]int {
+
+	if direction == 1 {
+		for i := 1; x-i >= 0; i++ {
+			if board[x-i][y] == player%2+1 {
+				board[x-i][y] = player
+			} else {
+				break
+			}
+		}
+	} else if direction == 2 {
+		for i := 1; i < 7; i++ {
+			if board[x-i][y+i] == player%2+1 {
+				board[x-i][y+i] = player
+			} else {
+				break
+			}
+		}
+	} else if direction == 3 {
+		for i := 1; i < 7; i++ {
+			if board[x][y+i] == player%2+1 {
+				board[x][y+i] = player
+			} else {
+				break
+			}
+		}
+	} else if direction == 4 {
+		for i := 1; i < 7; i++ {
+			if board[x+i][y+i] == player%2+1 {
+				board[x+i][y+i] = player
+			} else {
+				break
+			}
+		}
+	} else if direction == 5 {
+		for i := 1; i < 7; i++ {
+			if board[x+i][y] == player%2+1 {
+				board[x+i][y] = player
+			} else {
+				break
+			}
+		}
+	} else if direction == 6 {
+		for i := 1; i < 7; i++ {
+			if board[x+i][y-i] == player%2+1 {
+				board[x+i][y-i] = player
+			} else {
+				break
+			}
+		}
+	} else if direction == 7 {
+		for i := 1; y-i >= 0; i++ {
+			if board[x][y-i] == player%2+1 {
+				board[x][y-i] = player
+			} else {
+				break
+			}
+		}
+	} else if direction == 8 {
+		for i := 1; i < 7; i++ {
+			if board[x-i][y-i] == player%2+1 {
+				board[x-i][y-i] = player
+			} else {
+				break
+			}
+		}
+	}
+	return board
 }
 
 func Top(x int, y int, board [8][8]int, player int) bool {
